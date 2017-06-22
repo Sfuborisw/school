44f763b951cc6ca989aa1c10b54b8c594ad7271b
Franzi95 <franziska.lucas@gmx.de>
2017-06-22 12:52:47 +0200

    added wpfdyn

---
+++ Awe/Lotto/WpfDyn/WpfDyn/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDyn
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Button[] b;
+        public MainWindow()
+        {
+            InitializeComponent();
+            b = new Button[49];
+
+            for (int i = 0; i < 49; i++)
+            {
+                b[i] = new Button();
+                b[i].Name = "bt" + Convert.ToString(i + 1);
+                b[i].Height = 20;
+                b[i].Width = 20;
+                b[i].Background = Brushes.LightGray;
+                b[i].Content = Convert.ToString(i + 1);
+                WrapPanel1.Children.Add(b[i]);
+                b[i].Click += new RoutedEventHandler(bt_Click);
+            }
+
+        }
+    private void bt_Click(object sender, RoutedEventArgs e)
+        {
+            ((Button)sender).Background = Brushes.Red;
+            lb1.Content = lb1.Content + " - " + ((Button)sender).Content;
+        }
+    private void btClear_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 49; i++)
+            {
+                b[i].Background = Brushes.LightGray;
+            }
+            lb1.Content = "";
+        }
+    }
+}
