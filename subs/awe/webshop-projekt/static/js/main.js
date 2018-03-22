03bb1c114b659ebb36ef4df8ab531f03631ffab1
Maximilian Middeke <mam@baltic-online.de>
2018-03-22 17:17:30 +0100

    js cleanup

--- subs/awe/webshop-projekt/static/js/main.js
+++ subs/awe/webshop-projekt/static/js/main.js
@@ -1,9 +1,4 @@
 document.addEventListener('DOMContentLoaded', function(){ 
-    $.fn.extend({
-        toggleText: function(a, b){
-                return this.text(this.text() == b ? a : b);
-            }
-    });
     $('#register_container, #user_container').hide();
     $('#register').click(function(){
         $('#product_container, #shopping_cart').hide(300);
